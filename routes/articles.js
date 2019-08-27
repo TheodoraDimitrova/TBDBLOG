cd39f099b6b655c6b59c34f41a5f9a79dcc538bd
TheodoraDimitrova <teddybear901@abv.bg>
2019-08-27 13:32:31 +0300

    check

--- routes/articles.js
+++ routes/articles.js
@@ -1,71 +1,73 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const moment = require("moment");
-const isAuth=require('../config/isAuth')
+const moment = require('moment');
+const isAuth = require('../config/isAuth');
 
-Article = require("../models/Article");
-Category = require("../models/Category");
+Article = require('../models/Article');
+Category = require('../models/Category');
 
-router.get("/",isAuth, (req, res, next) => {
+router.get('/', isAuth, (req, res, next) => {
   Article.getArticles((err, articles) => {
     if (err) {
       console.log(err);
     }
     articles.forEach(article => {
       article.created_at = moment(article.created_on).format(
-        "MMMM Do YYYY, h:mm:ss a"
+        'MMMM Do YYYY, h:mm:ss a'
       );
-      article.date = moment(article.created_on).format("Do MMM");
+      article.date = moment(article.created_on).format('Do MMM');
     });
 
-    res.render("articles", { title: "All Articles", articles: articles });
+    res.render('articles', { title: 'All Articles', articles: articles });
   });
 });
-router.get("/show/:id",isAuth, (req, res, next) => {
+router.get('/show/:id', isAuth, (req, res, next) => {
   Article.getArticleById(req.params.id, (err, article) => {
     if (err) {
       console.log(err);
     }
-   
-    article.created_at = moment(article.created_on).startOf('day').fromNow();
-    let comments=article.comments;
- 
+
+    article.created_at = moment(article.created_on)
+      .startOf('day')
+      .fromNow();
+    let comments = article.comments;
+
     comments.forEach(comment => {
-   
-      if(comment.comment_author_id.toString()==req.user._id.toString() || req.user.role === "Admin"){
-        comment.comment_isAuthor=true;
-       }
-     
-      comment.comment_d=moment(comment.comment_date).format("Do MMMM YYYY")
-      comment.post_id=article._id
-      
+      if (
+        comment.comment_author_id.toString() == req.user._id.toString() ||
+        req.user.role === 'Admin'
+      ) {
+        comment.comment_isAuthor = true;
+      }
+
+      comment.comment_d = moment(comment.comment_date).format('Do MMMM YYYY');
+      comment.post_id = article._id;
     });
-  
-    res.render("article", { article: article ,message: req.flash()});
+
+    res.render('article', { article: article, message: req.flash() });
   });
 });
-router.get("/category/:category_id",isAuth, (req, res, next) => {
+router.get('/category/:category_id', isAuth, (req, res, next) => {
   Article.getCategoryArticles(req.params.category_id, (err, articles) => {
     Category.getCategoryById(req.params.category_id, (err, category) => {
       if (err) {
         console.log(err);
       }
       articles.forEach(article => {
-    
-        article.date = moment(article.created_on).format("Do MMM");
+        article.date = moment(article.created_on).format('Do MMM');
       });
-      res.render("articles", {
-        title: category.title + " Articles",
+      res.render('articles', {
+        title: category.title + ' Articles',
         articles: articles
       });
     });
   });
 });
-router.post("/edit/:id",isAuth, (req, res, next) => {
-  req.checkBody("title", "Title is required field").notEmpty();
-  req.checkBody("category", "Category is required field").notEmpty();
-  req.checkBody("content", "Content is required field").notEmpty();
-  req.checkBody("author", "Author is required field").notEmpty();
+router.post('/edit/:id', isAuth, (req, res, next) => {
+  req.checkBody('title', 'Title is required field').notEmpty();
+  req.checkBody('category', 'Category is required field').notEmpty();
+  req.checkBody('content', 'Content is required field').notEmpty();
+  req.checkBody('author', 'Author is required field').notEmpty();
 
   let errors = req.validationErrors();
   if (errors) {
@@ -77,8 +79,8 @@ router.post("/edit/:id",isAuth, (req, res, next) => {
         if (err) {
           res.sent(err);
         }
-        res.render("edit_article", {
-          title: "Edit Article",
+        res.render('edit_article', {
+          title: 'Edit Article',
           errors: errors,
           article: article,
           categories: categories
@@ -99,17 +101,16 @@ router.post("/edit/:id",isAuth, (req, res, next) => {
       if (err) {
         res.send(err);
       }
-      req.flash("success", "Article edited!");
-      res.redirect("/manage/articles");
+      req.flash('success', 'Article edited!');
+      res.redirect('/manage/articles');
     });
   }
 });
-router.post("/add",isAuth, (req, res, next) => {
-  
-  req.checkBody("title", "Title is required field").notEmpty();
-  req.checkBody("category", "Category is required field").notEmpty();
-  req.checkBody("content", "Content is required field").notEmpty();
-  req.checkBody("author", "Author is required field").notEmpty();
+router.post('/add', isAuth, (req, res, next) => {
+  req.checkBody('title', 'Title is required field').notEmpty();
+  req.checkBody('category', 'Category is required field').notEmpty();
+  req.checkBody('content', 'Content is required field').notEmpty();
+  req.checkBody('author', 'Author is required field').notEmpty();
 
   let errors = req.validationErrors();
   if (errors) {
@@ -117,8 +118,8 @@ router.post("/add",isAuth, (req, res, next) => {
       if (err) {
         console.log(err);
       }
-      res.render("add_article", {
-        title: "Create Article",
+      res.render('add_article', {
+        title: 'Create Article',
         errors: errors,
         categories: categories
       });
@@ -135,12 +136,12 @@ router.post("/add",isAuth, (req, res, next) => {
       if (err) {
         res.send(err);
       }
-      req.flash("success", "Article saved!");
-      res.redirect("/manage/articles");
+      req.flash('success', 'Article saved!');
+      res.redirect('/manage/articles');
     });
   }
 });
-router.delete("/delete/:id",isAuth, (req, res, next) => {
+router.delete('/delete/:id', isAuth, (req, res, next) => {
   const query = { _id: req.params.id };
   Article.removeArticle(query, (err, article) => {
     if (err) {
@@ -149,9 +150,9 @@ router.delete("/delete/:id",isAuth, (req, res, next) => {
     res.status(200);
   });
 });
-router.post("/comments/add:id",isAuth, (req, res, next) => {
-  req.checkBody("subject", "Subject is required field").notEmpty();
-  req.checkBody("comment", "Comment is required field").notEmpty();
+router.post('/comments/add:id', isAuth, (req, res, next) => {
+  req.checkBody('subject', 'Subject is required field').notEmpty();
+  req.checkBody('comment', 'Comment is required field').notEmpty();
   let errors = req.validationErrors();
   if (errors) {
     Article.getArticleById(req.params.id, (err, article) => {
@@ -159,7 +160,7 @@ router.post("/comments/add:id",isAuth, (req, res, next) => {
         console.log(err);
       }
 
-      res.render("article", { article: article, errors: errors });
+      res.render('article', { article: article, errors: errors });
     });
   } else {
     let article = new Article();
@@ -168,15 +169,15 @@ router.post("/comments/add:id",isAuth, (req, res, next) => {
     let comment = {
       comment_subject: req.body.subject,
       comment_body: req.body.comment,
-      comment_author:req.user.name,
-      comment_author_id:req.user._id
+      comment_author: req.user.name,
+      comment_author_id: req.user._id
     };
 
     Article.addComment(query, comment, (err, article) => {
       if (err) {
         console.log(err);
       }
-      res.redirect("/articles/show/" + req.params.id);
+      res.redirect('/articles/show/' + req.params.id);
     });
   }
 });
