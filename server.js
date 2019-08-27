fa71ab395d4e55048e27f99750fe27df141a2d37
TheodoraDimitrova <teddybear901@abv.bg>
2019-08-27 13:27:18 +0300

    up

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const $ = require("jquery");
-const session = require("express-session");
-const expressValidator = require("express-validator");
-const hbs = require("express-handlebars");
-const passport = require("passport");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const $ = require('jquery');
+const session = require('express-session');
+const expressValidator = require('express-validator');
+const hbs = require('express-handlebars');
+const passport = require('passport');
 
 const app = express();
 app.use(expressValidator());
 
 // Passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 // Express session
 app.use(
   session({
-    secret: require("./config/keys").secretOrKey,
+    secret: require('./config/keys').secretOrKey,
     resave: true,
     saveUninitialized: true
   })
@@ -33,25 +33,25 @@ app.use(passport.session());
 
 // Register Handlebars view engine
 app.engine(
-  ".hbs",
+  '.hbs',
   hbs({
-    defaultLayout: "main",
-    extname: ".hbs",
-    partialsDir: [__dirname + "/views/partials"]
+    defaultLayout: 'main',
+    extname: '.hbs',
+    partialsDir: [__dirname + '/views/partials']
   })
 );
-app.set("view engine", ".hbs");
+app.set('view engine', '.hbs');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Connection to DB
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, {
     useCreateIndex: true,
     useNewUrlParser: true
   })
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log('MongoDB connected'))
   .catch(() => {
     err => console.log(err);
   });
@@ -61,25 +61,25 @@ app.use(function(req, res, next) {
   if (req.isAuthenticated()) {
     res.locals.login = req.isAuthenticated();
     res.locals.user = req.user;
-    res.locals.is_admin = req.user.role === "Admin" ? true : false;
+    res.locals.is_admin = req.user.role === 'Admin' ? true : false;
   }
   next();
 });
 
 //Express Messages
-app.use(require("connect-flash")());
+app.use(require('connect-flash')());
 app.use((req, res, next) => {
-  res.locals.messages = require("express-messages")(req, res);
+  res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
 //Routes
-app.use("/", require("./routes/index"));
-app.use("/articles", require("./routes/articles"));
-app.use("/manage", require("./routes/manage"));
-app.use("/categories", require("./routes/categories"));
-app.use("/users", require("./routes/users"));
-app.use("/questions", require("./routes/questions"));
+app.use('/', require('./routes/index'));
+app.use('/articles', require('./routes/articles'));
+app.use('/manage', require('./routes/manage'));
+app.use('/categories', require('./routes/categories'));
+app.use('/users', require('./routes/users'));
+app.use('/questions', require('./routes/questions'));
 
 //Start server
 const port = process.env.PORT || 8000;
