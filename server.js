7b2b1b09cfd1b40db107efa9714643cd3c7fb350
TheodoraDimitrova <teddybear90@abv.bg>
2021-04-07 17:01:06 +0300

    remove_access_control

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acce
 
 const app = express();
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(expressValidator());
 
 // Passport Config
